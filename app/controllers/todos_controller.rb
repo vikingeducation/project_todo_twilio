f194c0bbefe798058796f99343f0b755b3ac5c6a
Adam Kinson <adam.kinson@gmail.com>
2015-07-08 13:50:44 -0400

    add Delete functionality to Index page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,6 @@
 class TodosController < ApplicationController
 
   def index
-    #t1 = Todo.new( {:task => "Task One", :completion_date => nil} )
-    #t2 = Todo.new( {:task => "Task Two", :completion_date => Date.today} )
-    #t3 = Todo.new( {:task => "Task Three", :completion_date => Date.today - 19} )
-    #t4 = Todo.new( {:task => "Task Four", :completion_date => nil} )
-    #t5 = Todo.new( {:task => "Task Five", :completion_date => Date.today - 12} )
-    #@test_todos = [t1, t2, t3, t4, t5]
-
     @todos = Todo.order(:completion_date => 'DESC')
     @new_task = Todo.new
   end
@@ -23,6 +16,13 @@ class TodosController < ApplicationController
       # fail flash.now
       render todos_path
     end
+  end
+
+
+  def destroy
+    @todo = Todo.find(params[:id]).destroy!
+    # flash success
+    redirect_to todos_path
   end
 
 
