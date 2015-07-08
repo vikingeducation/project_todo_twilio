710049abf1c5270c1d8b6e0956cd3b226e92d7b7
Adam Kinson <adam.kinson@gmail.com>
2015-07-08 11:24:27 -0400

    set up empty Todos index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "application#home"
 
+  resources :todos, :only => [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
