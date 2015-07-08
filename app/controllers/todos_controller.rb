c5748d302c422b98c7fefbafc39d2f149ef79c94
Adam Kinson <adam.kinson@gmail.com>
2015-07-08 11:48:33 -0400

    build hardcoded Todos for Index formatting

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,14 @@
 class TodosController < ApplicationController
 
   def index
+    t1 = Todo.new( {:task => "Task One", :completion_date => nil} )
+    t2 = Todo.new( {:task => "Task Two", :completion_date => Date.today} )
+    t3 = Todo.new( {:task => "Task Three", :completion_date => Date.today - 19} )
+    t4 = Todo.new( {:task => "Task Four", :completion_date => nil} )
+    t5 = Todo.new( {:task => "Task Five", :completion_date => Date.today - 12} )
+    @test_todos = [t1, t2, t3, t4, t5]
+
+    # @todos = Todo.order(:completion_date => 'DESC')
   end
 
 
