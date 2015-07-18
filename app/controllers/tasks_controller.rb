7547018f9d3fb1b69b96e2ad8498ab47f36b80ed
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-18 11:35:09 -0700

    adding crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,33 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-  
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_hash)
+
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    @task.completion_date = Time.now
+
+    @task.save
+
+    redirect_to tasks_path
+
+  end
+
+
+  private
+
+    def params_hash
+    params.require(:task).permit(:todo, :completion_date)
+    end
 end
