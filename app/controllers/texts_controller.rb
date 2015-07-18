e7fbe3b8dd5509b3734d001af2518db1748a0c48
shadefinale <shadefinale@gmail.com>
2015-07-18 13:07:43 -0500

    Working on twilio api usage.

---
+++ app/controllers/texts_controller.rb
@@ -0,0 +1,21 @@
+class TextsController < ApplicationController
+  def send_text
+    todo = Todo.find(params[:id])
+    sms_body = todo.goal + todo.completion_date.to_s
+    account_sid = ENV["TWILIO_SID"]
+    auth_token  = ENV["TWILIO_AUTH"]
+    from_number = ENV["TWILIO_NUMBER"] #+2345678912
+    to_number   = ENV["SELF_NUMBER"]
+    begin
+        client = Twilio::REST::Client.new account_sid, auth_token
+        client.account.messages.create(
+          from => from_number,
+          to =>   to_number,
+          body => sms_body
+        )
+    rescue Twilio::REST::RequestError => e
+        puts e.message
+    end
+    redirect_to todos_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root to: "todos#index"
   resources :todos
+  get '/texts/:id', to: 'texts#send_text'
 
 end
