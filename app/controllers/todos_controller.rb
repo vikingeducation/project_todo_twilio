6aa97b98abbb49eac3a3087638f07af029ee2613
Chris Scavello <bideowego@gmail.com>
2015-09-18 04:30:02 -0400

    Add todo CRUD

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,42 @@
 class TodosController < ApplicationController
-	before_action :set_todo, :except => [:index]
+	before_action :set_todo, :except => [:index, :create]
 
 	def index
-		@todos = Todo.all
+		@todos = Todo.all.order(:due)
 	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def create
+		@todo = Todo.new(todo_params)
+		if @todo.save
+			flash[:success] = 'Todo created!'
+			redirect_to @todo
+		else
+			flash.now[:error] = 'Error: todo not created'
+			render :index
+		end
+	end
+
+	def update
+		if @todo.update_attributes(todo_params)
+			flash[:success] = 'Todo updated!'
+			redirect_to @todo
+		else
+			flash.now[:error] = 'Error: todo not updated'
+			render :edit
+		end
+	end
+
+	def destroy
+		@todo.destroy
+		flash[:success] = 'Todo deleted!'
+		redirect_to todos_path
+	end 
 
 	private
 		def set_todo
