d7090386c43e65c3573c1b54d8b615ece51ecab9
Justin Mullis <justin.mullis@gmail.com>
2015-10-11 20:44:18 -0400

    remove row highlighting

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,7 +15,6 @@ class TodosController < ApplicationController
     @todo = Todo.new(todo_params)
     if @todo.save
       flash[:success] = "New Todo Created"
-      flash[:id] = @todo.id
       redirect_to todos_path
     else
       flash.now[:danger] = "Whoops, something went wrong."
@@ -36,7 +35,6 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
     if @todo.update(todo_params)
       flash[:info] = "Todo Updated"
-      flash[:id] = @todo.id
       redirect_to todos_path
     else
       flash.now[:danger] = "Whoops, something went wrong."
