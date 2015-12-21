361ce38cdd22fa4409c48e553d6d3261697f3ff6
Sia <siakaramalegos@gmail.com>
2015-12-21 12:51:47 -0600

    Modifies controller and views for cookies instead

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,14 @@
 class TodosController < ApplicationController
   def index
-    @todos = Todo.order(:due)
-    @todo = Todo.new
+    @todos = Todo.new(retrieve_todos).todo_list
+  end
+
+  def new
+  end
+
+  private
+
+  def retrieve_todos
+    cookies[:todo_list]
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,26 @@
 class Todo
+  attr_reader :todo_list
+
+  def initialize(todo_list = nil)
+    @todo_list = todo_list || []
+  end
+
+  def insert_todo(description, due)
+    # Step through sorted list right to left.
+    # As long as your due is less than the element
+    # at @todo_list[i] and you still have elements
+    i = @todo_list.length
+    while(i >= 0 && @todo_list[i].last > due)
+        # copy the element
+        @todo_list[i+1] = @todo_list[i]
+        i -= 1
+    end
+
+    # insert the actual element
+    @todo_list[i+1] = [description, due];
+
+    # return the @todo_list with the inserted element
+    @todo_list
+  end
+
 end
