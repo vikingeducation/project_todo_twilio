07e6e604a6ea3a3028c2ad88b790232f01bf605e
Sam O'Keefe <samok13@gmail.com>
2016-01-26 15:46:13 -0800

    finished CRUD-y todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,19 +1,48 @@
 class TodosController < ApplicationController
 
   def index
-    @todos = Todo.all
+    @todos = Todo.all.order(:date)
   end
 
   def create
     @todo = Todo.new(whitelist_todo_params)
+
     if @todo.save
       flash[:success] = "Your task has been added!"
+    else
+      flash[:error] = "The inputs can't be empty or input proper date!"
+    end
+     redirect_to root_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+
+    @todo.destroy
+    flash[:success] = "Your task has been deleted!"
+    redirect_to root_path
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+
+  def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(whitelist_todo_params)
+      flash[:success] = "Your task has been updated!"
       redirect_to root_path
     else
-      flash[:error] = "The input's can't be empty or input proper date!"
-      redirect_to root_path
+      flash[:error] = "The inputs can't be empty or input proper date!"
+      redirect_to :back
     end
+   
+
   end
+
+
 
   private
 
