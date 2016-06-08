46c256202484fcbcee43a6fedacfaaae499b6da9
Stephen Mayeux <stephenmayeux@gmail.com>
2016-06-09 02:20:55 +0900

    create tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,29 @@
 class TasksController < ApplicationController
 
     def index
-      @tasks = Task.order(deadline: :desc)
+      @tasks = Task.order(created_at: :desc)
     end
 
     def show
       @task = Task.find(params[:id])
+    end
+
+    def new
+      @task = Task.new
+    end
+
+    def create
+      @task = Task.new(white_listed_params)
+      if @task.save
+        redirect_to task_path(@task)
+      else
+        render 'new'
+      end
+    end
+
+    private
+    def white_listed_params
+      params.require(:task).permit(:title, :description, :deadline)
     end
 
 end
