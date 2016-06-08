08065a1fccf1bf298ded80547a22c37f81a7f03f
Stephen Mayeux <stephenmayeux@gmail.com>
2016-06-09 01:30:06 +0900

    show individual tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
-    
+
     def index
       @tasks = Task.order(deadline: :desc)
     end
-    
+
+    def show
+      @task = Task.find(params[:id])
+    end
+
 end
