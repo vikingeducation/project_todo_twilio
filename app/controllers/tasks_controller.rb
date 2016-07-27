ce15f81e2c1cf256104c5f4b65ece763cc73669d
Graham Turner <turnertgraham@gmail.com>
2016-07-27 13:08:20 -0400

    added new and show actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,19 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    raise
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :complete_date)
+  end
+
 end
