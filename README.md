9c5570514c54f6e2d050a3e108cdba2607aa7c95
Morgan Martin <morganm5201@gmail.com>
2016-07-27 11:58:27 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# todo_twilio
+# Morgan Martin and Leo Ahnn's todo_twilio
+
+A todo app with twilio api services
+
 
 [The Twilio API project on Ruby on Rails from the Viking Code School](http://www.vikingcodeschool.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectTodoTwilio::Application.config.secret_key_base = 'aa5908697d1881f58b40ef120ef7e04639da7d166034a06a34f23bb826dd6b57989a3c45c75118b7afdaab769af54f6ffbda489335072a2fe578530e8a23224b'
