af2036999296beaa41c28ebe8c7e87a98158d390
“Matthew <matthew.hinea@gmail.com>
2016-07-27 15:03:54 -0700

    debug figaro ENV variable troubles

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -3,8 +3,11 @@ require 'twilioapi'
 class TextsController < ApplicationController
   def send_text
     @task = Task.find(params[:task_id])
-    send_reminder(@task)
-
+    if send_reminder(@task)
+      flash[:success] = "A reminder was sent to your phone number."
+    else
+      flash[:error] = "Your phone number was wrong or something."
+    end
     redirect_to @task
   end
 
@@ -12,6 +15,6 @@ class TextsController < ApplicationController
 
   def send_reminder(task)
     client = TwilioAPI.new
-    client.text("Reminder!\n#{task.description} due at #{task.due_date.in_time_zone("Pacific Time (US & Canada)").to_s(:rfc822)}")
+    client.text("Reminder!\n#{task.description} due on #{task.due_date.in_time_zone("Pacific Time (US & Canada)").to_s(:rfc822)}")
   end
 end
