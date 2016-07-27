11f246dbbdf0277e3c2806ce151f2910c24f04da
David Jiang <davidjgrey@gmail>
2016-07-27 10:32:54 -0700

    adding flash messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,46 @@
 class TasksController < ApplicationController
 
+  def new
+    @task = Task.new
+  end
+
   def index
     @tasks = Task.all
     @tasks.sort_by {|task| task[:completion_date] }
   end
 
   def show
-    @task = Task.find_by(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "Task #{@task.title} was created"
+      redirect_to task_path(@task)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description,:completion_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks, :only => [:index, :show]
+  resources :tasks
   root 'tasks#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
