09386e0d0e95cbae82f79800b57cdf62ef77fd70
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-27 11:17:36 -0500

    added changes to controller and changed routes to resources

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+     if @task.save
+      redirect_to @task
+    else
+      redirect_to new_task_path
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      redirect_to @task
+    else
+      redirect_to edit_task_path(params[:id])
+    end
+
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require("task").permit("description")
+  end
+end
