d0ef04280d8d642495ad0330bc1791dfb8e8005a
Adrian Mui <adrian.mui@gmail.com>
2016-07-27 13:33:16 -0400

    fin destroy, edit inc

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,13 +14,30 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    raise
+    
+    if @task.save
+      flash[:notice] = "Task successfully created"
+      redirect_to @task
+    else 
+      flash.now[:error] = "Task was not created"
+      render :new
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      flash[:notice] = "Task succesfully destroyed"
+    else
+      flash[:error] = "Task was not deleted"
+    end
+    redirect_to root_path
   end
 
   private
 
-  def task_params
-    params.require(:task).permit(:title, :description, :complete_date)
-  end
+    def task_params
+      params.require(:task).permit(:title, :description, :complete_date)
+    end
 
 end
