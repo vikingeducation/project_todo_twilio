9ac593b82f0d32f90522facbf3a54cacf0fc48e4
David Jiang <davidjgrey@gmail>
2016-07-27 15:23:38 -0700

    added texting

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,9 +1,18 @@
 class TextsController < ApplicationController
 
   def send_text
-    texter = Texter.new(params[:title])
+    title = params[:title]
+    description = params[:description]
+    completion_date = params[:completion_date]
+    text = create_text(title, description, completion_date)
+    texter = Texter.new(text)
     texter.send_text
     redirect_to root_path
+  end
+
+  private 
+  def create_text(title, description, completion_date)
+    "Reminder: #{title}: #{description} is due on #{completion_date}."
   end
 
 end
--- app/models/Texter.rb
+++ app/models/Texter.rb
@@ -1,23 +1,22 @@
 require 'twilio-ruby'
-require_relative 'env'
 
 class Texter
 
-# account_sid = TWILIO_SID
-# auth_token = TWILIO_SECRET
-
-def initialize(text)
-  @client = Twilio::REST::Client.new "AC736c03bafc1edfe06f96e7df1e23e83d", "b9a609612a1a8b2ea3001a8392800520"
+ 
+def initialize(text) 
   @body = text
+  account_sid = ENV["TWILIO_SID"]
+  auth_token = ENV["TWILIO_TOKEN"]
+  @client = Twilio::REST::Client.new account_sid, auth_token
 end
 
 def send_text
   @client.messages.create(
-    from: '+16367353249',
-    to: "+16362931540",
+    from: ENV["TWILIO_PHONE"] ,
+    to: ENV["MY_PHONE"],
     body: @body
     )
-    # puts message.sid
+  
 end
 
 end
