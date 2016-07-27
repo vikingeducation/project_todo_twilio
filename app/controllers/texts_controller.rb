f6db569cd3f8d2678727b32e89a4bba8a5512237
jsteenb2 <jonathansteenbergen@gmail.com>
2016-07-27 15:01:11 -0700

    got twilio sending me messages

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,8 +1,9 @@
 class TextsController < ApplicationController
-  
+
   def send_text
     texter = Texter.new(params[:title])
     texter.send_text
+    redirect_to root_path
   end
 
 end
--- app/models/Texter.rb
+++ app/models/Texter.rb
@@ -1,21 +1,23 @@
 require 'twilio-ruby'
+require_relative 'env'
 
 class Texter
 
-account_sid = 
-auth_token = 
+# account_sid = TWILIO_SID
+# auth_token = TWILIO_SECRET
 
 def initialize(text)
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  @client = Twilio::REST::Client.new "AC736c03bafc1edfe06f96e7df1e23e83d", "b9a609612a1a8b2ea3001a8392800520"
   @body = text
 end
 
 def send_text
   @client.messages.create(
-    from:       ,
-    to:         ,
+    from: '+16367353249',
+    to: "+16362931540",
     body: @body
     )
+    # puts message.sid
 end
 
 end
