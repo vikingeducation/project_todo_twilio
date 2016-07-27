9f16fef159777038c3fc55942ec728fff420a70a
Leo Ahnn <leoahnn@gmail.com>
2016-07-27 13:22:38 -0400

    flashes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,8 +16,10 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(whitelisted_task_params)
     if @task.save
+      flash[:success] = "Task Saved!"
       redirect_to task_path(@task)
     else
+      flash[:alert] = "Task Not Saved!"
       render :new
     end
   end
@@ -28,6 +30,20 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(whitelisted_task_params)
+      flash[:success] = "Task Updated!"
+      redirect_to task_path(@task)
+    else
+      flash[:alert] = "Task Not Updated!"
+      render :edit
+    end
+  end
 
   private
 
