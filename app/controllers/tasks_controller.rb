0962f0d224830677e649bb2ec0f005c6ef65ffc9
Vishal Lama <vishal.lama@gmail.com>
2016-07-27 11:47:12 -0600

    Finish Creating Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,9 @@
 class TasksController < ApplicationController
 
+  def new
+    @task = Task.new
+  end
+
   def index
     @tasks = Task.order(deadline: :asc)
   end
@@ -8,4 +12,21 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def create
+    @task = Task.new(get_params)
+    
+    if @task.save
+      flash[:success] = "New task created!"
+      redirect_to @task
+    else
+      flash.now[:error] = "Task could not be created!"
+      render :new
+    end
+  end
+
+  private
+
+  def get_params
+    params.require(:task).permit(:title, :deadline, :description)
+  end
 end
