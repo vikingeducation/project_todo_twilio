350a32b6fc0603c28229a71bf07e07599a9b92fc
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-27 13:47:48 -0500

    solved button issue

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,14 @@
 class TextsController < ApplicationController
 
   def send_text
+    @client.messages.create(
+  from: '+14159341234',
+  to: '+16105557069',
+  body: 'Hey there!'
+)
+
+
+
     
   end
 end
