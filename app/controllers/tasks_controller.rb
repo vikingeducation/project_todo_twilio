9dba19b938af06188c1e730dc2af112fedd7ba0a
Mike Lee <asackofwheat@gmail.com>
2016-07-27 14:14:54 -0400

    working edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,10 +16,10 @@ class TasksController < ApplicationController
     @task = Task.new(get_params)
     
     if @task.save
-      flash[:success] = "New task created!"
+      flash[:Success] = "New task created!"
       redirect_to @task
     else
-      flash.now[:error] = "Task could not be created!"
+      flash.now[:Error] = "Task could not be created!"
       render :new
     end
   end
@@ -28,11 +28,27 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     if @task.destroy
-      flash[:success] = "Task deleted!"
+      flash[:Success] = "Task deleted!"
     else
-      flash[:error] = "Task could not be deleted! Try again."
+      flash[:Error] = "Task could not be deleted! Try again."
     end
     redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(get_params)
+      flash[:Success] = "Task updated!"
+      redirect_to @task
+    else
+      flash.now[:Error] = "Task could not be updated! Try again."
+      render edit_task_path(@task)
+    end
+    
   end
 
   private
