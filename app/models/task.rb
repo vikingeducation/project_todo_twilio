139a51ef3fe341de21a0c18a00c42ea312f754b9
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-27 20:01:22 -0400

    Do optional assignments

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,20 +2,20 @@ class Task < ActiveRecord::Base
   validates :description, length: { minimum: 4 }
 
   def self.sort_priority_asc
-    self.order(:sticky => :desc, priority: :desc)
+    self.order({:sticky => :desc}, {priority: :desc})
   end
 
   def self.sort_priority_desc
-    self.order(:sticky => :desc, :priority)
+    self.order({:sticky => :desc}, :priority)
   end
 
   def self.sort_description
-    self.order(:sticky => :desc, :description)
+    self.order({:sticky => :desc}, :description)
   end
 
   def self.sort_completion_date
 
-    self.order(:sticky => :desc, :completion_date)
+    self.order({:sticky => :desc}, :completion_date)
   end
 
 
