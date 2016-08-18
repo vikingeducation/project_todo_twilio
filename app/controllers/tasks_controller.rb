937c2920509fd4f5c997fbaf45679556ebc18bc9
BranLiang <lby89757@hotmail.com>
2016-08-18 10:19:52 +0800

    add the flash and create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,5 +12,18 @@ class TasksController < ApplicationController
   end
 
   def create
+    new_task = Task.new(whitelist_params)
+    if new_task.save
+      flash[:success] = "yeah, new task created!"
+      redirect_to task_path(new_task)
+    else
+      flash.now[:danger] = "Something wrong!"
+      render :new
+    end
   end
+
+  private
+    def whitelist_params
+      params.require(:task).permit(:title, :description, :due_date)
+    end
 end
