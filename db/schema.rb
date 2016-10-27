ecfacbc318079c2569f415b20a8d2ae2b77241fb
Joe Bernardi <joebernardi@hotmail.com>
2016-10-26 20:49:41 -0500

    move to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161026004756) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.datetime "date"
