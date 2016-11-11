3c79db42db44eb3810b954cd770f8f116bbc702d
Conor Fischer <conor.fischer@gmail.com>
2016-11-10 18:51:05 -0600

    Added Space

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161103014344) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
