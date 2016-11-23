a41d32e33128fa5e6e707535570a1d0093ab79fb
Jake Tingley <jaket32@gmail.com>
2016-11-23 13:09:49 -0500

    delete works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,24 @@ class TasksController < ApplicationController
   end
 
   def create
-
+    @task = Task.new(whitelist_params)
+    if @task.save
+      redirect_to @task
+    else
+      #error message
+    end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def whitelist_params
+    params.require(:task).permit(:title, :description, :due_date)
+  end
 end
