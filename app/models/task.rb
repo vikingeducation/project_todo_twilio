73e38bdfae3b3fc9cdc4b62cbb647123dddda18b
Pramod Jacob <psjacob68@gmail.com>
2016-11-23 13:12:21 -0500

    working on date validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,14 @@
 class Task < ApplicationRecord
-  validates_date :completion_date,
+
+  validate :valid_completion_date
+  validates :completion_date, presence: true
+
+  def valid_completion_date
+    begin
+      !!Date.parse(:completion_date)
+    rescue
+      errors.add(:completion_date, "Invalid date!")
+    end
+  end
+
 end
