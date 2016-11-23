de0e8d4d693b0553430c2b5cad4801c0f0e1c6d7
Francisco Suero <suerofrancisco@yahoo.com>
2016-11-23 13:09:16 -0500

    calendar woes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,21 @@
 class TasksController < ApplicationController
 
   def index
+    @task = Task.new
     @tasks = Task.order(:due)
   end
 
   def show
     @task = Task.find_by_id(params[:id])
+  end
+
+  def create
+    Task.new(strong_params).save
+    redirect_to tasks_url
+  end
+
+  def strong_params
+    params.require(:task).permit(:due, :description)
   end
 
 end
