0950ce96beeeacbc17474be0bc5ea2981d959824
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-23 10:37:57 -0800

    delete, validation, and flash messages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,17 +15,34 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(whitelist_params)
     if @task.save
+      flash[:success] = "Task created!"
       redirect_to @task
     else
-      #error message
+      flash.now[:error] = "Unable to save!"
+      render 'new'
     end
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-
+    flash[:success] = "Task deleted!"
     redirect_to root_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(whitelist_params)
+      flash[:success] = "Your task has been updated!"
+      redirect_to @task
+    else
+      flash.now[:error] = "There were problems with your update!"
+      render 'edit'
+    end
   end
 
   private
