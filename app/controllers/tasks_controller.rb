90bef41f7582cdb60430b77d9ddf8d145b4b1544
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-23 12:09:20 -0600

    completed restful routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,27 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to :root
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      flash[:success] = "Task updated!"
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+
   private
 
   def task_params
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :description, length: { minimum: 4 }
+  validate :completion_date_cannot_be_in_the_past
+
+  def completion_date_cannot_be_in_the_past
+    if completion_date.present? && completion_date < Date.today
+      errors.add(:completion_date, "can't be in the past")
+    end
+  end
+
+
 end
