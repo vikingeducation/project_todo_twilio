0904afc82a4f6e411ddf1420ceea9c5c8b7c913a
Christian Carey <christianrcarey@gmail.com>
2016-11-23 12:41:16 -0500

    save new tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,27 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "Task saved."
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :completion_date)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |n|
+  Task.create(description: "Task #{n}", completion_date: Time.now + 1000)
+end
