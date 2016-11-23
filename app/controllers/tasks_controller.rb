c42cad5e7eb63e9f9cde189d4ec379b1a5a89f16
Dexter Ford <dexter.ford.77@gmail.com>
2016-11-23 12:39:20 -0600

    Add new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,25 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = Task.find_by_id(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+    else
+      flash[:error] = "Try again son!"
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:due, :additional, :description)
+  end
 
 end
