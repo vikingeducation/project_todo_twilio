739321b952996c7c8a76f8c24c95b0dfa11e3b7b
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-23 11:05:36 -0800

    Start edit action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,29 @@ class TasksController < ApplicationController
     @task = Task.find_by_id(params[:id])
   end
 
+  def edit
+    #
+  end
+
+  def update
+    #
+  end
+
   def new
     @task = Task.new
+  end
+
+  def destroy
+    @task = Task.find_by_id(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
+      flash[:success] = "Task created!"
+      redirect_to @task
     else
       flash[:error] = "Try again son!"
     end
