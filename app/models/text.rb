d24b58d684d7a3b2d7fb8288f25fb80b7805bea6
Kenny Kottenstette <kennykotten@gmail.com>
2016-11-27 21:37:48 -0800

    bootstrap switch

--- app/models/text.rb
+++ app/models/text.rb
@@ -7,13 +7,3 @@ class Text
     auth_token = ENV['TOKEN_KEY']
     @client = Twilio::REST::Client.new account_sid, auth_token
   end
-
-  def send_text
-    @client.messages.create(
-      from: ENV['FROM_KEY'],
-      to: ENV['TO_KEY'],
-      body: "Task: #{@description}, is due: #{@due_date}"
-    )
-  end
-
-end
