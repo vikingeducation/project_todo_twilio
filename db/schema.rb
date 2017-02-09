f243275c64186d0e4728d1e6f0cc5c594b23e373
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-09 19:26:31 +0800

    Add ability to view all existing tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209110810) do
+ActiveRecord::Schema.define(version: 20170209113132) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "description"
     t.date     "deadline"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "title"
   end
 
 end
