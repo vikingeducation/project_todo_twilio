34c3fa7d04ee4291124defa69fbf742984e47b45
yxlau <yixuan.lau@gmail.com>
2017-02-10 08:32:36 +0800

    Add ability to update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,35 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline)
+  end
 end
