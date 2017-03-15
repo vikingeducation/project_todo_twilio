0ba5a3d3a91ae58cdcd241563574f7629d9ef05c
Visiona <darbis@gmail.com>
2017-03-15 22:56:52 +0000

    Created New Task action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,22 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(whitelisted_params)
+    @task.save
+
+    render :show
+  end
+
+
+  private
+
+  def whitelisted_params
+    params.require(:task).permit(:description, :completion_date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root "tasks#index"
 
-  resources :tasks, :only => [:index, :show]
+  resources :tasks, :only => [:index, :show, :new, :create]
 
 
 end
