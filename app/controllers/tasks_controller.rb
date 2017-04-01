f4c9f1e3d50c4e98792136ef8255af0e687af943
efraimmgon <efraimmaugusto@gmail.com>
2017-04-01 19:30:58 -0400

    Creating tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,18 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to @task
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :required_completion_date)
+  end
+
 end
