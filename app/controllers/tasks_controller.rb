0155f45e6a9d015d5f57343d376ddcac412907d0
Frank Yu <frankyu1@gmail.com>
2017-04-26 16:27:33 -0700

    added delete task feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
     def task_params
