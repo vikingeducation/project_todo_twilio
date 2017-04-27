193ab9c68409758c92aaa0438f3ba374b9863a43
Frank Yu <frankyu1@gmail.com>
2017-04-26 17:18:39 -0700

    added edit and update feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,19 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
