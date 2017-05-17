fd0c8345bcf64596099ed80261c30dbc7b6f179e
Ann Allan <ann.cristina.allan@gmail.com>
2017-05-16 21:52:47 -0400

    Add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,18 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(white_listed_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  private
+  def white_listed_params
+    params.require(:task).permit(:title, :description, :completion_date)
+  end
 end
