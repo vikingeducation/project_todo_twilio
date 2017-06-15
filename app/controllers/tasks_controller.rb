0c9476fa5b54b5e2c4a16673af9705da7b5d1a87
Roy Chen <roychenlei@gmail.com>
2017-06-15 17:31:04 +0800

    completed Creating Tasks (Create) story

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,25 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "Your task was successfully saved!"
+      redirect_to @task
+    else
+      flash.now[:notice] = "Your task had errors. Please correct them and try again."
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :due_date)
+  end
 end
