7c585afc976907b8d705095d3dc5e519bcaa5a1a
andie5 <andieasa@googlemail.com>
2017-07-10 23:08:35 +0100

    created model , seeded with 10 tasks and home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Destroy all existing posts
+Task.destroy_all
+
+# Create 10 sample posts
+10.times do |i|
+  t = Task.new
+  t.title = "Task #{i+1}"
+  t.description = "This is the task for the project X"
+  t.owner = "Owner #{i+1}"
+  t.effort = rand(20)
+  t.save!
+end
