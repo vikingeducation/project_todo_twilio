72c6254ac8eb7ddf4668287e8c89d841830d7111
Sean Kelly <shonkelli@gmail.com>
2017-08-24 23:25:17 -0400

    added destroy route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,4 +18,10 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
 end
