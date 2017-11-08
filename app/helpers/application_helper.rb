8e11e113c19483d0163d63bdcee4cc0afa6d9255
anne <richardson.ae@gmail.com>
2017-11-08 16:19:23 -0600

    Move flashes to partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def bootstrap_flash_class(type)
+    case type
+    when 'error' then 'warning'
+    when 'success' then 'success'
+    else
+      'info'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'tasks#index'
-  resources :tasks, only: [:index, :show, :new, :create]
+  resources :tasks, only: [:index, :show, :new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
