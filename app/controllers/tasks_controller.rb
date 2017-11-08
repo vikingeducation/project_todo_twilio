70c984858d52f3f6eb51518576fb5ec2a97dc721
anne <richardson.ae@gmail.com>
2017-11-08 15:18:08 -0600

    Install pry

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,15 @@
 class TasksController < ApplicationController
   def index
+    # @tasks = Task.all.order('completed_on DESC')
+    @tasks = Task.completed_tasks.by_date
+    @completed_points = Task.calculate_complete_points
+    @total_points = Task.calculate_total_points
+    @percent_complete = Task.calculate_percent_complete
+
   end
 
   def show
+    @task = Task.find(params['id'])
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,22 @@
 class Task < ApplicationRecord
+
+  def self.completed_tasks
+    where.not(completed_on: nil)
+  end
+
+  def self.by_date
+    order('completed_on DESC')
+  end
+
+  def self.calculate_complete_points
+    completed_tasks.pluck('point_value').reduce(&:+)
+  end
+
+  def self.calculate_total_points
+    all.pluck('point_value').reduce(&:+)
+  end
+
+  def self.calculate_percent_complete
+    (calculate_complete_points.to_f / calculate_total_points.to_f * 100.00).to_i
+  end
 end
