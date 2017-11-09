3f68f5b93cedbd9fb488102dd9e5240198cb01e2
anne <richardson.ae@gmail.com>
2017-11-08 20:04:49 -0600

    Adds categories to task form, view, and controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -56,7 +56,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :point_value, :completed_on)
+    params.require(:task).permit(:name, :point_value, :completed_on, :category_id)
   end
 
   def set_task
