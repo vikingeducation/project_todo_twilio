1df4bc5f7fda6be052bec264000babf5b8ff7a9a
anne <richardson.ae@gmail.com>
2017-11-09 10:26:22 -0600

    Provides sample seeds for production launch

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.find_or_create_by!(name: 'Sample Category')
+  Task.create!([
+    {category_id: Category.last.id, name: "Sample Task 1", point_value: 30, started_on: Date.parse('01/06/2017'), completed_on: Date.parse('26/06/2017')},
+    {category_id: Category.last.id, name: "Sample Task 2", point_value: 50, started_on: Date.parse('26/06/2017'), completed_on: Date.parse('05/07/2017')},
+    {category_id: Category.last.id, name: "Sample Task 3", point_value: 20, started_on: Date.parse('10/07/2017'), completed_on: nil},
+    {category_id: Category.last.id, name: "Sample Task 4", point_value: 40, started_on: nil, completed_on: nil},
+  ])
