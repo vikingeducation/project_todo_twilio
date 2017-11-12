75f1289badcba15378c2eae7c9d77718671df40a
anne <richardson.ae@gmail.com>
2017-11-12 10:10:50 -0600

    Calculates velocity based on the last 3 tasks completed

--- app/models/task_shared.rb
+++ app/models/task_shared.rb
@@ -1,17 +1,23 @@
 module TaskShared
 
+  def current_velocity(proc = Proc.new { completed })
+    # calculates an average from the last 3 tasks
+    tasks = most_recently_completed_tasks(3, proc)
+    velocities = []
+    tasks.each do |task|
+      days = calculate_days_taken(task)
+      velocity = (task.point_value / days).to_f
+      velocities << velocity
+    end
+    @current_velocity ||= velocities.reduce(&:+) / velocities.length
+  end
+
+  def most_recently_completed_tasks(qty, proc)
+    proc.call.order('completed_on DESC').to_a.shift(qty)
+  end
+
   def calculate_days_taken(task)
     days = (task.completed_on - task.started_on).to_i
     days == 0 ? 1 : days # compensates for same-day completion
-  end
-
-  def current_velocity(proc = Proc.new { completed })
-    task = most_recently_completed_task(proc)
-    days = calculate_days_taken(task)
-    @current_velocity ||= (task.point_value / days).to_f
-  end
-
-  def most_recently_completed_task(proc)
-    proc.call.order('completed_on DESC').first
   end
 end
